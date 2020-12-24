6e0f4ac5b3951b0ceb11b453640ca286d0a46be8
elmomani <ahmad.mom.20000@gmail.com>
2020-12-24 21:54:37 +0200

    added swap box with example

--- lib/screens/dashboard/swap_screen.dart
+++ lib/screens/dashboard/swap_screen.dart
@@ -1,10 +1,51 @@
+import 'package:dinkum_app/models/article.dart';
+import 'package:dinkum_app/services/firestore_service.dart';
+import 'package:dinkum_app/shared/constants.dart';
+import 'package:dinkum_app/widgets/category_chip.dart';
+import 'package:dinkum_app/widgets/swap/swipe_box.dart';
 import 'package:flutter/material.dart';
 
-class SwapScreen extends StatelessWidget {
+import '../../widgets/article_card.dart';
+
+class SwapScreen extends StatefulWidget {
+  @override
+  _SwapScreenState createState() => _SwapScreenState();
+}
+
+class _SwapScreenState extends State<SwapScreen> {
+  List<String> selectedCategories = [];
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Swap Screen"),
+    var vw = MediaQuery.of(context).size.width;
+    var vh = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {});
+        },
+        child: FutureBuilder<List<Article>>(
+          future: FirestoreService().getArticles(selectedCategories),
+          builder: (_, snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error);
+              return Center(child: Text("Something went wrong"));
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Container(
+                width: double.infinity,
+                height: 400,
+                child: SwipeBox(
+                  children: (snapshot.data.map((e) => ArticleCard(e))).toList(),
+                  onSwipeRight: () {},
+                ),
+              );
+            }
+
+            return Center(child: CircularProgressIndicator());
+          },
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/swap/swipe_box.dart
@@ -0,0 +1,136 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SwipeBox extends StatefulWidget {
+  //TODO add comments to this code and make it generic
+  final List<Widget> children;
+  final Function onSwipeRight;
+  final Color color;
+  SwipeBox({
+    @required this.children,
+    this.onSwipeRight,
+    this.color = Colors.blue,
+  });
+
+  @override
+  _SwipeBoxState createState() => _SwipeBoxState();
+}
+
+class _SwipeBoxState extends State<SwipeBox> {
+  int _currentDeg = 0;
+  bool _needFix = false;
+  int get _currentIdx {
+    if (_currentDeg <= 0) return 0;
+    return (_currentDeg / 180.0).round();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragUpdate: (val) {
+        setState(() {
+          print("CurrentDeg================${_currentDeg % 360}");
+
+          if (_currentDeg % 180 >= 60 && _currentDeg % 180 <= 120) {
+            //move faster between 90'
+            _currentDeg += -val.delta.dx.toInt() * 7;
+          } else
+            _currentDeg += -val.delta.dx.toInt();
+
+          if (_currentDeg >= (widget.children.length - 1) * 180)
+            //max index boundry
+            _currentDeg = (widget.children.length - 1) * 180;
+          else if (_currentDeg <= -60) _currentDeg = -60; //min index boundry
+
+          //fixs mirror effect on 90'
+          _needFix = (_currentDeg / 180).round() % 2 == 1; //
+        });
+      },
+      onHorizontalDragEnd: (_) => setState(() {
+        if (_currentDeg < 0) {
+          //users swipe right on zero index
+          if (_currentDeg <= -50)
+            //call function if reached threshold
+            widget.onSwipeRight();
+          // go back to home place and cancel
+          else
+            _currentDeg = 0;
+        } else if ((_currentDeg % 180) <= 90) {
+          //if degree is less than 90'
+          //go back
+          _currentDeg -= (_currentDeg % 90);
+        } else {
+          //degree >90' so go forward
+          _currentDeg = 90 * (_currentDeg / 90).ceil();
+        }
+
+        //no need for this check but I WILL KEEP IT
+        _needFix = (_currentDeg / 180) % 2 == 1;
+      }),
+      child: _currentDeg < 0
+          ? // put stack on swap right
+          _currentDeg < -50
+              //max threshold is -50
+              ? buildSwipeStack(-50)
+              : buildSwipeStack(_currentDeg)
+          //3d transform
+          : buildTransformY(
+              _currentDeg,
+              //this transform do the swipe effect
+              buildTransformY(
+                _needFix ? 180 : 0,
+                //this transform fixs the mirror effect
+                widget.children[_currentIdx],
+              ),
+            ),
+    );
+  }
+
+  Stack buildSwipeStack(int xOffset) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Container(
+            color: widget.color,
+            //   width: double.infinity,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Icon(
+                    Icons.swap_horiz,
+                    size: 50,
+                  ),
+                  Text(
+                    "تبديل",
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned.fill(
+          child: Transform.translate(
+            offset: Offset(xOffset * -1.0, 0),
+            child: widget.children[0],
+          ),
+        )
+      ],
+    );
+  }
+
+  Transform buildTransformY(int degree, Widget child) {
+    return Transform(
+        alignment: FractionalOffset.center,
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.001)
+          ..rotateY(degree * pi / 180.0),
+        child: child);
+  }
+}
