71b6234cababe239c2ba14179580b1837abc94c9
elmomani <ahmad.mom.20000@gmail.com>
2020-12-25 21:17:11 +0200

    parallax appbar for article screen

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -7,37 +7,91 @@ class ArticleScreen extends StatelessWidget {
   ArticleScreen(this.article);
   @override
   Widget build(BuildContext context) {
+    var vw = MediaQuery.of(context).size.width;
+    var vh = MediaQuery.of(context).size.height;
+    Color color = article_ccolors[article.categories[0]];
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: article_ccolors[article.categories[0]],
-        title: Text(article.title),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 200,
-                width: double.infinity,
-                child: Image.network(
-                  article.featuredImg,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                article.content,
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: color,
+              leading: IconButton(
+                  icon: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 1.0,
+                        top: 2.0,
+                        child: Icon(Icons.arrow_back, color: color),
+                      ),
+                      Icon(Icons.arrow_back, color: Colors.white),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }),
+              title: Text(
+                article.title,
                 style: TextStyle(
                   fontFamily: "Comfortaa",
+                  fontSize: 20,
+                  //?  background: Paint()..color = Colors.blue,
+                  shadows: [
+                    Shadow(color: color, offset: Offset(2, 2), blurRadius: 0)
+                  ],
                 ),
               ),
-            ],
+              centerTitle: true,
+              automaticallyImplyLeading: false,
+              expandedHeight: .35 * vh,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  //titlePadding: EdgeInsets.only(bottom: 0),
+
+                  collapseMode: CollapseMode.parallax,
+                  background: Image.network(
+                    article.featuredImg,
+                    width: vw,
+                    //height: .35 * vh - 10,
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          ];
+        },
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(article.content),
           ),
         ),
       ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return true;
   }
 }
